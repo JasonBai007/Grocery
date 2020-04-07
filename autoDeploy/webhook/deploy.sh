cb163832b37341c05957d1373d064789026593b9
Jason Bai <byygkcg@163.com>
2020-04-07 10:01:20 +0800

    Update deploy.sh

--- autoDeploy/webhook/deploy.sh
+++ autoDeploy/webhook/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-
-# 要执行的命令，拉取仓库最新代码，编译，删除原来的文件，复制最新静态文件
+# 进入到仓库目录，拉取最新代码，编译，删除原来的文件，复制最新静态文件
 cd ../projectRepository
 git pull
 npm run build
 rm -rf /home/projectFolderName/*
-cp -r /home/projectFolderName-deployment/projectRepository/dist/* /home/v2x
+cp -r /home/projectRepository/dist/* /home/projectFolderName
