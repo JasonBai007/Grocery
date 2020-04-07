3e44be4d28d0c24e41742e0228326e6ecdaa7571
Jason Bai <byygkcg@163.com>
2020-04-07 10:00:12 +0800

    Update server.js

--- autoDeploy/webhook/server.js
+++ autoDeploy/webhook/server.js
@@ -1,15 +1,14 @@
 // 以下是在服务器上运行的监听github的webhook的服务
 // 需要在github上开启webhook
-// 在服务器上使用forever持久化这个服务（pm2不管用！）
+// 在服务器上使用forever持久化这个服务（pm2有问题！）
 // forever start server.js
 // forever logs 0
 // forever restart server.js
-
 var path = require("path");
 var http = require("http");
 var cp = require("child_process");
 var webhookHandler = require("github-webhook-handler");
-var handler = webhookHandler({ path: "/", secret: "byy" });
+var handler = webhookHandler({ path: "/", secret: "your secret" });
 // 部署脚本路径
 var shellPath = path.resolve(__dirname, "./deploy.sh");
 
